303bc7d5a7e3214db507fd36141be615e3a216b4
Henri Meijer <henri@ezfeed.nl>
2026-05-08 14:04:28 +0200

    Finished Lab: Fetch Data Using useFetch

--- custom_hook/src/Components/UseFetch.jsx
+++ custom_hook/src/Components/UseFetch.jsx
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const UseFetch = (url) => {
-    
+
+  // const apiUrl = 'https://api.npoint.io/9045c260b1565daa9e15';
+
+  // fetch(apiUrl)
+  //   .then(response => response.json())
+  //   .then(data => {console.log(data);})
+  //   .catch(error => {console.error('Error fetching data:', error);});
+
+  const[data,setData]=useState();
+
+  useEffect(()=>{
+    fetch(url).then((res)=>res.json())
+    .then((data)=>setData(data))
+  },[])
+  return [data];
+
 }
 
 export default UseFetch
